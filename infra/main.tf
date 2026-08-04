daa868576ebcaa6f2f3ec59feee4dc74bd16ed75
Alisson <alissonmotasep@gmail.com>
2026-08-04 15:09:49 -0300

    fix: corrigir referencia de modulos e state key apos reorganizacao

--- infra/main.tf
+++ infra/main.tf
@@ -2,7 +2,7 @@
 # SQS Queue
 ################################################################################
 module "sqs_dynamodb" {
-  source = "../../toggle-master-infra/modules/sqs_dynamodb"
+  source = "git::https://github.com/alissonmota-am/toggle-master-infra.git//modules/sqs_dynamodb?ref=develop"
 
   project_name = var.project_name
   queue_name   = var.sqs_queue_name
