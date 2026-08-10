84afe2b11261516a5d269bc4a7005413180c88a3
Alisson <alissonmotasep@gmail.com>
2026-08-10 12:29:43 -0300

    test

--- infra/main.tf
+++ infra/main.tf
@@ -1,10 +1,19 @@
 ################################################################################
 # SQS Queue
 ################################################################################
-module "sqs_dynamodb" {
-  source = "git::https://github.com/alissonmota-am/toggle-master-infra.git//modules/sqs_dynamodb?ref=develop"
+module "sqs" {
+  source = "git::https://github.com/alissonmota-am/toggle-master-infra.git//modules/sqs?ref=develop"
 
-  project_name = var.project_name
-  queue_name   = var.sqs_queue_name
-  table_name   = var.dynamodb_table_name
+  queue_name = var.sqs_queue_name
+}
+
+################################################################################
+# DynamoDB Table
+################################################################################
+module "dynamodb" {
+  source = "git::https://github.com/alissonmota-am/toggle-master-infra.git//modules/dynamodb?ref=develop"
+
+  table_name    = var.dynamodb_table_name
+  hash_key      = "event_id"
+  hash_key_type = "S"
 }
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,19 +1,19 @@
 output "sqs_queue_url" {
   description = "URL da fila SQS (usar em AWS_SQS_URL)"
-  value       = module.sqs_dynamodb.queue_url
+  value       = module.sqs.queue_url
 }
 
 output "sqs_queue_arn" {
   description = "ARN da fila SQS"
-  value       = module.sqs_dynamodb.queue_arn
+  value       = module.sqs.queue_arn
 }
 
 output "dynamodb_table_name" {
   description = "Nome da tabela DynamoDB"
-  value       = module.sqs_dynamodb.table_name
+  value       = module.dynamodb.table_name
 }
 
 output "dynamodb_table_arn" {
   description = "ARN da tabela DynamoDB"
-  value       = module.sqs_dynamodb.table_arn
+  value       = module.dynamodb.table_arn
 }
